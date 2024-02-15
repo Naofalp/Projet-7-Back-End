416ac8e5fc987a41c6586775c240004dc3fb34e8
NaoP <naop18@yahoo.fr>
2024-02-16 00:48:31 +0100

    ajout headers erreur CORS

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -2,24 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Requête reçue !');
+app.use((req, res, next) => { //Ajout de headers pour gerer les erreurs CORS et demande d API. Pas d'adresse en parametres = s'applique à toutes les routes
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
-
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
-});
-
 
 module.exports = app;
