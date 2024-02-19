5a7d88cfe31b2edc832ffc9cb5a362c2b64017a5
NaoP <naop18@yahoo.fr>
2024-02-19 23:27:27 +0100

    creation du fichier schema

---
+++ BackEnd/models/book.js
@@ -0,0 +1,13 @@
+ /* 
+  A adapter selon le model a suivre
+ const mongoose = require('mongoose');
+
+const bookSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Thing', thingSchema); */
