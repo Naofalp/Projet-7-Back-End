3ba75d44a219224b8c6e0acc1ce9e1d48b98eba9
NaoP <naop18@yahoo.fr>
2024-02-19 23:02:15 +0100

    connexion à mongodb

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://utilisateur-test:utilisateur-test@cluster0.yxicz2r.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 const app = express();
 
 app.use((req, res, next) => { //Ajout de headers pour gerer les erreurs CORS et demande d API. Pas d'adresse en parametres = s'applique à toutes les routes
