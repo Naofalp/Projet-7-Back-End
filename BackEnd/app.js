48f726592b24d337947dfd0da0a10321f2692eeb
NaoP <naop18@yahoo.fr>
2024-02-27 12:48:43 +0100

    création de signup et login

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express(); //remplace body.parser
+const app = express();
 
+//importation des routes
 const userRoutes = require('./routes/user');
-const user = require('./models/user');
 
 mongoose.connect('mongodb+srv://utilisateur-test:utilisateur-test@cluster0.yxicz2r.mongodb.net/?retryWrites=true&w=majority')
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+app.use(express.json()); // remplace le body.parser
 
 app.use((req, res, next) => { //Ajout de headers pour gerer les erreurs CORS et demande d API. Pas d'adresse en parametres = s'applique à toutes les routes
     res.setHeader('Access-Control-Allow-Origin', '*');
--- BackEnd/controllers/user.js
+++ BackEnd/controllers/user.js
@@ -1,7 +1,43 @@
-exports.signup = (req, res, next) => {
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+const User = require('../models/user');
+
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10) // Recupere le Password du model User et le Hash 10 fois 
+        .then(hash => {
+            const user = new User({ //Création de l'user qui va servir à stocker et comparer dans login
+                email: req.body.email,
+                password: hash
+            });
+            user.save() //Enregistrement de User dans la BDD
+                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error })); //Si erreur de fonctionnement
 };
 
 exports.login = (req, res, next) => {
-
+    User.findOne({ email: req.body.email }) // Vérifie e-mail user rentré correspond à existant de la bdd
+        .then(user => {
+            if (!user) { //Si l'email correspond pas 401 unautorized
+                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+            }
+            bcrypt.compare(req.body.password, user.password) // si email correspond : compare le mdp avec le hash enregistré dans la bdd
+                .then(valid => {
+                    if (!valid) { // si correspond pas 
+                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                    }
+                    res.status(200).json({ //Si correspond alors renvoi une réponse 200 contenant l'ID utilisateur et un token. 
+                        userId: user._id,
+                        token: jwt.sign( //fonction sign de JWT pour creer un nveau token
+                            { userId: user._id }, //ID de l'utilisateur en tant que payload 
+                            'RANDOM_TOKEN_SECRET', //clé secrète pour chiffrer un token
+                            { expiresIn: '24h' }
+                        )
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error })); // SI erreur de fonctionnement
 };
