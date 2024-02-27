f0efcea382294847ef67b38d916ee70572d2aa85
NaoP <naop18@yahoo.fr>
2024-02-27 12:25:25 +0100

    model fait

--- BackEnd/models/book.js
+++ BackEnd/models/book.js
@@ -1,13 +1,22 @@
- /* 
-  A adapter selon le model a suivre
- const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const bookSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  imageUrl: { type: String, required: true },
   userId: { type: String, required: true },
-  price: { type: Number, required: true },
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  year: { type: Number, required: true },
+  genre: { type: String, required: true },
+  rating: [{
+    userId: { type: String, required: true },
+    grade: { type: Number, required: true },
+  }],
+  averageRating: { type: Number, required: true }
 });
 
-module.exports = mongoose.model('Thing', thingSchema); */
+module.exports = mongoose.model('Book', bookSchema);
+
+/*
+Book = le nom du modéle.
+La base de données MongoDB est fractionnée en collections : 
+le nom de la collection est défini par défaut sur le pluriel du nom du modèle. Ici, ce sera Books */
