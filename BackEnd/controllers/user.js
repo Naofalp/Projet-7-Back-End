fd7d3b2248f7d36120c62ded8a3e7a2041ba7cbc
NaoP <naop18@yahoo.fr>
2024-02-27 12:50:19 +0100

    commentaires

--- BackEnd/controllers/user.js
+++ BackEnd/controllers/user.js
@@ -6,29 +6,29 @@ const User = require('../models/user');
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10) // Recupere le Password du model User et le Hash 10 fois 
         .then(hash => {
-            const user = new User({ //Création de l'user qui va servir à stocker et comparer dans login
+            const user = new User({ 
                 email: req.body.email,
                 password: hash
             });
-            user.save() //Enregistrement de User dans la BDD
+            user.save() 
                 .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
                 .catch(error => res.status(400).json({ error }));
         })
-        .catch(error => res.status(500).json({ error })); //Si erreur de fonctionnement
+        .catch(error => res.status(500).json({ error })); 
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email }) // Vérifie e-mail user rentré correspond à existant de la bdd
+    User.findOne({ email: req.body.email })
         .then(user => {
-            if (!user) { //Si l'email correspond pas 401 unautorized
+            if (!user) { 
                 return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
             }
-            bcrypt.compare(req.body.password, user.password) // si email correspond : compare le mdp avec le hash enregistré dans la bdd
+            bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
-                    if (!valid) { // si correspond pas 
+                    if (!valid) {
                         return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
                     }
-                    res.status(200).json({ //Si correspond alors renvoi une réponse 200 contenant l'ID utilisateur et un token. 
+                    res.status(200).json({ 
                         userId: user._id,
                         token: jwt.sign( //fonction sign de JWT pour creer un nveau token
                             { userId: user._id }, //ID de l'utilisateur en tant que payload 
@@ -39,5 +39,5 @@ exports.login = (req, res, next) => {
                 })
                 .catch(error => res.status(500).json({ error }));
         })
-        .catch(error => res.status(500).json({ error })); // SI erreur de fonctionnement
+        .catch(error => res.status(500).json({ error })); 
 };
