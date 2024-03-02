aeae28baaeb466736790c75cb9025fcc33ac94ed
NaoP <naop18@yahoo.fr>
2024-03-02 22:11:30 +0100

    CONFIG de multer

--- BackEnd/middleware/multer-config.js
+++ BackEnd/middleware/multer-config.js
@@ -1,1 +1,21 @@
 const multer = require('multer');
+
+
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png'
+};
+Ò
+const storage = multer.diskStorage({ //méthode configure le chemin et le nom de fichier pour les fichiers entrants.
+    destination: (req, file, callback) => {
+        callback(null, 'images');
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(' ').join('_'); // on supprime les espaces pour remplacer par des _
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name + Date.now() + '.' + extension);
+    }
+});
+
+module.exports = multer({ storage: storage }).single('image');
