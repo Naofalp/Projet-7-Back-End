c7fd529a9cc6ac673dcda5efa8da14063f1780d2
NaoP <naop18@yahoo.fr>
2024-03-02 22:13:23 +0100

    implementation de fs pour creer ou pas dossier images

--- BackEnd/middleware/multer-config.js
+++ BackEnd/middleware/multer-config.js
@@ -1,5 +1,5 @@
 const multer = require('multer');
-
+const fs = require('fs');
 
 const MIME_TYPES = {
     'image/jpg': 'jpg',
@@ -9,7 +9,11 @@ const MIME_TYPES = {
 Ò
 const storage = multer.diskStorage({ //méthode configure le chemin et le nom de fichier pour les fichiers entrants.
     destination: (req, file, callback) => {
-        callback(null, 'images');
+        const dir = 'images';
+        if (!fs.existsSync(dir)) { // si pas de dossier images: en creer un
+            fs.mkdirSync(dir);
+        }
+        callback(null, dir);
     },
     filename: (req, file, callback) => {
         const name = file.originalname.split(' ').join('_'); // on supprime les espaces pour remplacer par des _
