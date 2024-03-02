c26a2fb31888f41d4dcce9d10637269a2600d676
NaoP <naop18@yahoo.fr>
2024-03-02 19:36:50 +0100

    route api/books/:id

--- BackEnd/controllers/stuff.js
+++ BackEnd/controllers/stuff.js
@@ -5,3 +5,9 @@ exports.getAllBooks = (req, res, next) => {
         .then(things => res.status(200).json(things))
         .catch(error => res.status(400).json({ error }));
 };
+
+exports.getOneBook = (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })   // Objet de comparaison : cherche celui dont l'id est = à l'id envoyé dans les parametres de requete URL
+        .then(thing => res.status(200).json(thing))
+        .catch(error => res.status(404).json({ error }));
+};
--- BackEnd/routes/stuff.js
+++ BackEnd/routes/stuff.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
-
 const stuffCtrl = require('../controllers/stuff');
+
 
 router.get('/', auth, stuffCtrl.getAllBooks);
 
+router.get('/:id', auth, stuffCtrl.getOneBook);
 
 module.exports = router;
