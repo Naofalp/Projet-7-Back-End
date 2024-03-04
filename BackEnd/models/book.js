33f108380504e0bd78022a1add350ecfd863d5ee
NaoP <naop18@yahoo.fr>
2024-03-04 08:00:06 +0100

    import de la fonction average

--- BackEnd/models/book.js
+++ BackEnd/models/book.js
@@ -11,8 +11,20 @@ const bookSchema = mongoose.Schema({
     userId: { type: String },
     grade: { type: Number },
   }],
-  averageRating: { type: Number }
+  averageRating: { type: Number, default: 0 }
 });
+
+//calcul de la note moyenne
+bookSchema.pre('save', function (next) {
+  const ratings = this.ratings.map((rating) => rating.grade)
+  if (ratings.length === 0) {
+    this.averageRating = 0
+  } else {
+    const sumOfRatings = ratings.reduce((acc, curr) => acc + curr, 0)
+    this.averageRating = Math.ceil(sumOfRatings / ratings.length)
+  }
+  next()
+})
 
 module.exports = mongoose.model('Book', bookSchema);
 
