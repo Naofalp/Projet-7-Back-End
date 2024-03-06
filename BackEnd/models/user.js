6aaa215f0809fe5b89b17f7e4fd76ee2544c4432
NaoP <naop18@yahoo.fr>
2024-03-06 08:44:59 +0100

    condition respect RGPD

--- BackEnd/models/user.js
+++ BackEnd/models/user.js
@@ -3,10 +3,20 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      // Validator vérifie si le password contient au moins une majuscule, un caractère spécial et un chiffre.
+      validator: function (value) {
+        return /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/.test(value);
+      },
+      message: 'Votre mot de passe doit contenir au moins une majuscule, un caractère spécial, un chiffre et avoir une longueur minimale de 12 caractères.',
+      errorCode: 555 // Code d'erreur personnalisé sinon 400 par defaut.
+    }
+  }
 });
 
-//mot de passe à 12 caractere sinon pas autorié. respecter RGPD au moins un caratere special etc. 
 
 userSchema.plugin(uniqueValidator); // la valeur unique =true , avec l'élément mongoose-unique-validator passé comme plug-in, s'assurera que deux utilisateurs ne puissent partager la même adresse e-mail.
 
