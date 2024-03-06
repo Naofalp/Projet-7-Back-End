26143f81bfef06e1f9fbdd8fa8025943a171356a
NaoP <naop18@yahoo.fr>
2024-03-06 08:32:05 +0100

    clé token dans .env

--- BackEnd/controllers/user.js
+++ BackEnd/controllers/user.js
@@ -1,26 +1,30 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const dotenv = require('dotenv'); // sert a lire le dossier env.
+dotenv.config();
+const TokenKey = process.env.TokenKey
+
 const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10) // Recupere le Password du model User et le Hash 10 fois 
         .then(hash => {
-            const user = new User({ 
+            const user = new User({
                 email: req.body.email,
                 password: hash
             });
-            user.save() 
+            user.save()
                 .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
                 .catch(error => res.status(400).json({ error }));
         })
-        .catch(error => res.status(500).json({ error })); 
+        .catch(error => res.status(500).json({ error }));
 };
 
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
-            if (!user) { 
+            if (!user) {
                 return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
             }
             bcrypt.compare(req.body.password, user.password)
@@ -28,16 +32,17 @@ exports.login = (req, res, next) => {
                     if (!valid) {
                         return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
                     }
-                    res.status(200).json({ 
+                    res.status(200).json({
                         userId: user._id,
                         token: jwt.sign( //fonction sign de JWT pour creer un nveau token
                             { userId: user._id }, //ID de l'utilisateur en tant que payload 
-                            'RANDOM_TOKEN_SECRET', //clé secrète pour chiffrer un token
+                            TokenKey, //clé secrète pour chiffrer un token
                             { expiresIn: '24h' }
                         )
                     });
+                    
                 })
                 .catch(error => res.status(500).json({ error }));
         })
-        .catch(error => res.status(500).json({ error })); 
+        .catch(error => res.status(500).json({ error }));
 };
--- BackEnd/middleware/auth.js
+++ BackEnd/middleware/auth.js
@@ -2,13 +2,18 @@
 //de connexion aux différentes méthodes qui vont gérer les requêtes
 
 const jwt = require('jsonwebtoken');
+
+const dotenv = require('dotenv'); // sert a lire le dossier env.
+dotenv.config();
+const TokenKey = process.env.TokenKey
  
 module.exports = (req, res, next) => {
    try {
         //extraire l'userId du token chiffré
        const token = req.headers.authorization.split(' ')[1]; //diviser la string en un tableau pour n'avoir que le token qui est en deuxième
-       const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); //fonction verify pour décoder notre token. Si celui-ci n'est pas valide, une erreur sera générée.
+       const decodedToken = jwt.verify(token, TokenKey); //fonction verify pour décoder notre token. Si celui-ci n'est pas valide, une erreur sera générée.
        const userId = decodedToken.userId; // recup userId qu'on avait mis dans le token dans le controller
+       console.log(token);
        //Rajouter l'id à la requete afin que les différentes routes puissent l’exploiter.
        req.auth = {
            userId: userId
